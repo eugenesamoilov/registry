df9d754be9b0e6c00d7793792a66a603af79dfc5
eugenesamoilov <eugene.samoilov@gmail.com>
2020-03-10 10:34:57 +0900

    a

---
+++ localmachine/start.sh
@@ -0,0 +1,3 @@
+cp ./daemon.json /etc/docker/
+service docker restart
+
---
+++ remove_all_repo.sh
@@ -0,0 +1,4 @@
+rm -rf ./data/* && echo "deleted /data/"
+rm -rf ./data_private/*  && echo "deleted /data_private"
+docker-compose restart
+
